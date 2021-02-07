87c46a0db475e5e7299676d275a8965055e02c8b
Eric Tao <erictao04@gmail.com>
2021-02-07 12:18:51 -0500

    Environment

--- My_Portfolio/settings.py
+++ My_Portfolio/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import dj_database_url
 from pathlib import Path
 import os
-import dotenv
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,15 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-dotenv_file = os.path.join(BASE_DIR, ".env")
-if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
 
-SECRET_KEY = os.environ['MY_PORTFOLIO_SECRET_KEY']
+SECRET_KEY = config(
+    'SECRET_KEY', default="fxj9@)$b%fl7i2cz5g_jb_28+@i05)&$zy0j%$uu8z5nx^6#wb")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
