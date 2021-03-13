5758502b4f44723a130a20f2a84113998a694ea7
erictao04 <erictao04@gmail.com>
2021-03-13 11:13:33 -0500

    New JS and Sticky Nav

--- static/js/downloads.js
+++ static/js/downloads.js
@@ -1,8 +1,10 @@
 const export_form = document.querySelectorAll('form');
 
 let export_icon;
+let download_link = document.querySelector('.download_link');
 for (let i=0; i<export_form.length; i++) {
     export_form[i].addEventListener('submit', function() {
+        download_link.style.display = 'none';
         export_icon = document.querySelector(`.export > li:nth-child(${i+1}) i:nth-child(2)`);
         let check;
         for (let j = 0; j<export_form.length; j++) {
---
+++ static/js/nav_bar.js
@@ -0,0 +1,21 @@
+const button = document.querySelector('#check')
+const section = document.querySelectorAll('section');
+let display = true;
+
+button.addEventListener('click', function() {
+    if (display) {
+        section.forEach(remove_content)
+    } else {
+        section.forEach(add_content)
+    }
+})
+
+function remove_content(section) {
+    section.style.display = 'none';
+    display = false;
+}
+
+function add_content(section) {
+    section.style.display = 'block';
+    display = true;
+}
