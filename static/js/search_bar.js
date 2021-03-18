260994fd239a2770a076d9f6d2b9075b34188245
erictao04 <erictao04@gmail.com>
2021-03-17 21:21:17 -0400

    Search Bar

---
+++ static/js/search_bar.js
@@ -0,0 +1,30 @@
+const searchBar = document.querySelector('.search-bar');
+const list = document.querySelector('section ul');
+let listItem;
+let itemText;
+let inputValue;
+
+searchBar.addEventListener('change', function() {
+    resetItems();
+    inputValue = searchBar.value;
+
+    for (let i = 0; i<list.childElementCount; i++) {
+        listItem = list.children[i];
+        itemText = listItem.textContent;
+        
+        removeItem(listItem, itemText);
+    }
+})
+
+function resetItems() {
+    for (let i = 0; i<list.childElementCount; i++) {
+        listItem = list.children[i];
+        listItem.style.display = 'flex'
+    } 
+}
+
+function removeItem(item, text) {
+    if (text.toLowerCase().indexOf(searchBar.value.toLowerCase()) === -1) {
+        item.style.display = 'None';
+    } 
+}
