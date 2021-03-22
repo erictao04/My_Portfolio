21c06fe07a3219fedabbdb2bba09b98ec7742c1b
erictao04 <erictao04@gmail.com>
2021-03-21 22:18:41 -0400

    Mobile menu + test

--- static/js/nav_bar.js
+++ static/js/nav_bar.js
@@ -1,11 +1,16 @@
 const button = document.querySelector('#check')
 const section = document.querySelectorAll('section');
+const side_checkbtns = document.querySelectorAll('.side-check')
+
 
 button.addEventListener('click', function() {
     if (button.checked) {
-        section.forEach(remove_content)
+        section.forEach(remove_content);
     } else {
-        section.forEach(add_content)
+        section.forEach(add_content);
+        for (let i = 0; i < side_checkbtns.length; i++) {
+            side_checkbtns[i].checked = false;
+        }
     }
 })
 
@@ -16,4 +21,3 @@ function remove_content(section) {
 function add_content(section) {
     section.style.display = 'block';
 }
-
