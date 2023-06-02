fe83ffa284797d95153a2debe8e5ff8938d0320a
Eric Tao <erictao04@gmail.com>
2023-06-01 21:58:15 -0400

    Remove PDF option
    Issues with pandas (used in pdf export) when deploying to fly.io

--- sports/views.py
+++ sports/views.py
@@ -100,12 +100,7 @@ def season(request, league_name, team_name, season_year):
             if league.name == "NHL":
                 link = export_nhl("xlsx")
             return render_link("xlsx")
-
-        elif "pdf_script" in request.POST:
-            if league.name == "NHL":
-                link = export_nhl("pdf_html")
-            return render_link("pdf_html")
-
+        
         elif "csv_script" in request.POST:
             if league.name == "NHL":
                 link = export_nhl("csv")
